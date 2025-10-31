08e77e7dcf60943fd362698a051927bfb1d3d831
Marcos Castro Abarca <61596615+bymac-si@users.noreply.github.com>
2025-10-31 00:27:50 -0300

    fix: definir alias @views para Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Repo: santajosefina-vue3
-const base = process.env.VITE_BASE || '/santajosefina-vue3/'
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
+  base: '/santajosefina-vue3/'   // 👈 importante para GitHub Pages
 })
